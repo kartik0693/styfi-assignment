08c9ac2e0e7af45fd9cc9ef741d8125570305c96
Kartik <kartik0693@gmail.com>
2017-04-09 16:12:29 +0530

    - First Commit

---
+++ app/src/main/java/com/styfi/assignment/ManufacturerListActivity.java
@@ -0,0 +1,13 @@
+package com.styfi.assignment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ManufacturerListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manufacturer_list);
+    }
+}
